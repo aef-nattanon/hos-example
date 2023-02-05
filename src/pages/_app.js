0a886086d1c53e3fce1eb7d84b717d8db1673eec
aefnattanon <aef.nattanon@gmail.com>
2023-02-05 15:54:40 +0700

    Add init redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
+import { Provider } from "react-redux";
+import store from "../store";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return <Provider store={store}>
+       <Component {...pageProps} />
+  </Provider>
 }
