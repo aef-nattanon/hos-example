12270407851f786bb5542a1fc02725e59fdfd4bf
aefnattanon <aef.nattanon@gmail.com>
2023-02-05 16:02:15 +0700

    Add redux-persist

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,17 @@
 import '@/styles/globals.css'
 import { Provider } from "react-redux";
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import store from "../store";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Provider store={store}>
-       <Component {...pageProps} />
-  </Provider>
+
+  let persistor = persistStore(store);
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<>loading.........</>} persistor={persistor}>
+        <Component {...pageProps} />
+        </PersistGate>
+    </Provider>
+  )
 }
--- src/store.js
+++ src/store.js
@@ -1,8 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import counterReducer from './features/counter/counterSlice'
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-})
+
+const persistConfig = {
+  key: "hos-enterprise",
+  storage,
+  version: 1,
+  // whitelist: [],
+}
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+
+export function makeStore() {
+  return configureStore({
+    reducer: persistReducer(persistConfig, rootReducer),
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ serializableCheck: false }),
+  });
+}
+
+export default  makeStore();
